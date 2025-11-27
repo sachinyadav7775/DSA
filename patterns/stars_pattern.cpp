c688115328bcc869d5e619970dfb62dfc4e56c32
sachin yadav <sachinyadav63928@gmail.com>
2025-11-27 07:58:33 +0530

    add new paragraph

--- patterns/stars_pattern.cpp
+++ patterns/stars_pattern.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int n = 5;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {                                           // {1}
+    int x = 5;
+    for(int i=1; i<=x; i++) {
+        for(int j=1; j<=x; j++) {                                           // {1}
             cout<< "* ";
         }
         cout<<endl;
     }
     cout<<endl;
 
-    // Triangle pattern 
+    // [ Triangle pattern ]
 
-    int m = 4;
-    for(int i=0; i<m; i++) {
+    int n = 4;
+    for(int i=0; i<n; i++) {
         for(int j=0; j<i+1; j++) {                                          // {2}
             cout<<"* ";
         }
@@ -21,7 +21,40 @@ int main() {
     }
     cout<<endl;
 
-    // Pyramid pattern
+    int N = 4;
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<=N-1-i; j++) {                                       // {3}
+            cout << "* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    int m = 4;
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<m-i; j++) {
+            cout <<" ";
+        }                                                                   // {4}
+        for(int j=0; j<=i; j++) {
+            cout <<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    int M = 4;
+    for(int i=M; i>=1; i--) {
+        for(int j=0; j<M-i; j++) {
+            cout<<" ";
+        }                                                                   // {5}
+        for(int j=0; j<i; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    // [ Pyramid pattern ]
 
     int r = 8;
     for(int i=0; i<r; i++) {
@@ -29,7 +62,7 @@ int main() {
         for(int j=0; j<r-1-i; j++) {
             cout << " ";
         }
-        // number 1                                                          // {3}
+        // number 1                                                          // {6}
         for(int j=1; j<=i+1; j++) {
             cout << "*";
         }
@@ -42,10 +75,10 @@ int main() {
     }
     cout<<endl;
 
-    // Diamond pattern
+    // [ Diamond pattern ]
 
     int u = 4;
-    // Top
+    // Top part
     for(int i=0; i<u; i++) {
 
         // Spaces
@@ -53,7 +86,7 @@ int main() {
             cout << " ";
         }
         
-        cout << "*";                                                         
+        cout << "*";                                                        
                                                         
         if(i != 0) {
             // Spaces
@@ -63,9 +96,8 @@ int main() {
             cout << "*";
         }
         cout<<endl;
-    }
-                                                                            
-    // bottom                                                              // {4}
+    }                                                             
+    // Bottom part                                                             // {7}
     for(int i=0; i<u-1; i++) {
 
         // Spaces
@@ -81,6 +113,37 @@ int main() {
                 cout << " ";
             }
             cout << "*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    // [ Butterfly Pattern ]
+
+    int R = 4;
+    // Top part
+    for(int i=0; i<R; i++) {
+        for(int j=0; j<i+1; j++) {                                        
+            cout<<"*";
+        }
+        for(int j=0; j<2*(R-i-1); j++) {
+            cout <<" ";
+        }
+        for(int j=0; j<=i; j++) {
+            cout <<"*";
+        }
+        cout<<endl;
+    }                                                                           // {8}
+    // Bottom part
+    for(int i=R-1; i>=0; i--) {
+        for(int j=0; j<i+1; j++) {
+            cout <<"*";
+        }
+        for(int j=0; j<2*(R-i-1); j++) {
+            cout<<" ";
+        }
+        for(int j=0; j<i+1; j++) {
+            cout<<"*";
         }
         cout<<endl;
     }
