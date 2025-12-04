5a3833ece44c144d8659f63d5ae6d931a6fa39c9
sachin yadav <sachinyadav63928@gmail.com>
2025-12-04 11:53:57 +0530

    add new file

---
+++ Bitwise_Operator/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 6 , b = 4;
+    cout << (a & b) <<endl;
+    cout << (a | b) <<endl;
+    cout << (a ^ b) <<endl;
+    cout << (8 << 2) <<endl;
+    cout << (4 >> 1) <<endl;
+
+    return 0;
+}
