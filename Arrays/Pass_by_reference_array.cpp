fac68c727ccf1dcfbce172ed35cfa34b171a7400
sachin yadav <sachinyadav63928@gmail.com>
2025-12-05 20:09:13 +0530

    paragraph

---
+++ Arrays/Pass_by_reference_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void changeArr(int arr[], int Size) {
+    cout << "in function\n";
+    for(int i=0; i<Size; i++) {
+        arr[i] = 2*arr[i];
+    }
+}
+int main() {
+    int Arr[] = { 1, 2, 3 };
+    changeArr(Arr, 3);
+    cout << "in main\n";
+    for(int i=0; i<3; i++) {
+        cout << Arr[i] << " ";
+        
+    }
+    cout<<endl;
+
+    return 0;
+}
