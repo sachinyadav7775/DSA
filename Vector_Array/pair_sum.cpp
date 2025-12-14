0bc11fa2a02e2945ae9d609e6a086d8016a9bf67
sachin yadav <sachinyadav63928@gmail.com>
2025-12-14 05:58:42 +0530

    add new file

---
+++ Vector_Array/pair_sum.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int>pairSum(vector<int> nums, int target) {
+    vector<int> ans;
+    int n = nums.size();
+    // for(int i=0; i<n; i++) {
+    //     for(int j=i+1; j<n; j++) {
+    //         if(nums[i] + nums[j] == target) {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+
+    int i = 0, j = n-1;
+    while(i < j) {
+        int pairSum = nums[i] + nums[j] ;
+        if(pairSum > target){
+            j--;
+        } else if(pairSum < target) {
+            i++;
+        } else {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int target = 11;
+    vector<int>nums = {4, 5, 8, 2, 7};
+
+    vector<int> ans = pairSum(nums, target);
+    cout << ans[0] << " , " << ans[1] <<endl;
+
+    return 0;
+}
